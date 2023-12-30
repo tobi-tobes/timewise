d85cb6d80b9a26f381afedb3bceb64a5eb8e8879
Tobi Tijani <tobi_tijani@yahoo.com>
2023-12-30 15:19:57 +0100

    Moved all-time stats functionality to content script

--- js/content-script.js
+++ js/content-script.js
@@ -5,6 +5,52 @@
 chrome.storage.local.get(['totalBreakTime', 'totalTimeSpent', 'totalSessions'], function(data) {
   console.log(data);
 
+  // Update stats dashboard with total number of sessions
   const totalSessions = document.querySelector('.total-sessions .time');
-  totalSessions.textContent = data.totalSessions;
+  let totalSessionsCount = data.totalSessions || 0;
+  totalSessions.textContent = totalSessionsCount;
+
+  // Update stats dashboard with total time
+  const totalTime = document.querySelector('.total-time .time');
+  let totalTimeSpent = data.totalTimeSpent || 0;
+  if (totalTimeSpent < 60) {
+    totalTime.textContent = `${totalTimeSpent} mins`;
+  } else if (totalTimeSpent === 60) {
+    totalTime.textContent = '1 h';
+  } else {
+    const minutes = totalTimeSpent % 60;
+    const hours = Math.floor(totalTimeSpent / 60);
+    totalTime.textContent = `${hours} h ${minutes} mins`;
+  }
+
+  // Update stats dashboard with total break time taken
+  const totalBreakTime = document.querySelector('.total-break-time .time');
+  let totalBreakTimeSpent = data.totalBreakTime || 0;
+  const totalBreakTimeSpentInMins = Math.ceil(totalBreakTimeSpent / (1000 * 60));
+  if (totalBreakTimeSpentInMins < 60) {
+    totalBreakTime.textContent = `${totalBreakTimeSpentInMins} mins`;
+  } else if (totalBreakTimeSpentInMins === 60) {
+    totalBreakTime.textContent = '1 h';
+  } else {
+    const minutes = totalBreakTimeSpentInMins % 60;
+    const hours = Math.floor(totalBreakTimeSpentInMins / 60);
+    totalBreakTime.textContent = `${hours} h ${minutes} mins`;
+  }
+
+  // Update stats dashboard with average session length
+  const avgSessionLength = document.querySelector('.avg-session-length .time');
+  if (totalSessionsCount == 0) {
+    avgSessionLength.textContent = '0 mins';
+  } else {
+    const average = totalTimeSpent / totalSessionsCount;
+    if (average < 60) {
+      avgSessionLength.textContent = `${average} mins`;
+    } else if (average === 60) {
+      avgSessionLength.textContent = '1 h';
+    } else {
+      const minutes = average % 60;
+      const hours = Math.floor(average / 60);
+      avgSessionLength.textContent = `${hours} h ${minutes} mins`;
+    }
+  }
 });
--- js/stats.js
+++ js/stats.js
@@ -3,10 +3,6 @@
 
 $(document).ready(function() {
   // ALL-TIME STATS SECTION FUNCTIONALITY
-  const totalSessions = document.querySelector('.total-sessions .time');
-  const totalTime = document.querySelector('.total-time .time');
-  const totalBreakTime = document.querySelector('.total-break-time .time');
-  const avgSessionLength = document.querySelector('.avg-session-length .time');
 
   // Listen for messages from the extension
   window.addEventListener('message', function(event) {
@@ -14,51 +10,6 @@ $(document).ready(function() {
       console.log(event.data.data);
     }
   });
-
-  // Update stats dashboard with total time
-  let totalTimeSpent = localStorage.getItem('totalTimeSpent') || 0;
-  if (totalTimeSpent < 60) {
-    totalTime.textContent = `${totalTimeSpent} mins`;
-  } else if (totalTimeSpent === 60) {
-    totalTime.textContent = '1 h';
-  } else {
-    const minutes = totalTimeSpent % 60;
-    const hours = Math.floor(totalTimeSpent / 60);
-    totalTime.textContent = `${hours} h ${minutes} mins`;
-  }
-
-  // Update stats dashboard with total number of sessions
-//   let totalSessionsCount = localStorage.getItem('totalSessions') || 0;
-//   totalSessions.textContent = `${totalSessionsCount}`;
-
-  // Update stats dashboard with total break time taken
-  let totalBreakTimeSpent = localStorage.getItem('totalBreakTime') || 0;
-  const totalBreakTimeSpentInMins = Math.ceil(totalBreakTimeSpent / (1000 * 60));
-  if (totalBreakTimeSpentInMins < 60) {
-    totalBreakTime.textContent = `${totalBreakTimeSpentInMins} mins`;
-  } else if (totalBreakTimeSpentInMins === 60) {
-    totalBreakTime.textContent = '1 h';
-  } else {
-    const minutes = totalBreakTimeSpentInMins % 60;
-    const hours = Math.floor(totalBreakTimeSpentInMins / 60);
-    totalBreakTime.textContent = `${hours} h ${minutes} mins`;
-  }
-
-  // Update stats dashboard with average session length
-//   if (totalSessionsCount == 0) {
-//     avgSessionLength.textContent = '0 mins';
-//   } else {
-//     const average = totalTimeSpent / totalSessionsCount;
-//     if (average < 60) {
-//       avgSessionLength.textContent = `${average} mins`;
-//     } else if (average === 60) {
-//       avgSessionLength.textContent = '1 h';
-//     } else {
-//       const minutes = average % 60;
-//       const hours = Math.floor(average / 60);
-//       avgSessionLength.textContent = `${hours} h ${minutes} mins`;
-//     }
-//   }
 
   // DAILY STATS FUNCTIONALITY
   // Get current date to display on dashboard
